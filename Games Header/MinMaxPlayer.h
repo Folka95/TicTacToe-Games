ddbcefa841edbb6169a04262f1ee0aa527602831
Mohamed Mahmoud <162477460+DevilMO05@users.noreply.github.com>
2024-12-15 19:30:53 +0200

    Add files via upload

--- Games Header/MinMaxPlayer.h
+++ Games Header/MinMaxPlayer.h
@@ -1,52 +1,61 @@
-#ifndef _MINMAXPLAYER_H
-#define _MINMAXPLAYER_H
+#ifndef _WORD_MINMAX_PLAYER_H
+#define _WORD_MINMAX_PLAYER_H
 
 #include "BoardGame_Classes.h"
 #include <bits/stdc++.h>
-using namespace std;
+
 
 template <typename T>
-class Pyramid_MinMax_Player : public Player<T> {
-public:
-    Pyramid_MinMax_Player(T symbol);
-    ~Pyramid_MinMax_Player();
-    void getmove(int& x, int& y) override;
+class Word_MinMax_Player : public Player<T> {
+    const unordered_set<string>& dictionary;
+    vector<T> allSymbols;
 
-private:
-    int calculateMinMax(T s, bool isMaximizing);
-    pair<int, int> getBestMove();
+    int calculateMinMax(T symbol, bool isMaximizing);
+    pair<int, int> getBestMove(T symbol);
+    bool isWinningMove(int x, int y, T symbol);
+    bool tryRandomMove(int& x, int& y);
+    bool isWordFormed();
+    std::vector<string> getLines();
+
+public:
+    Word_MinMax_Player(T symbol, const unordered_set<string>& dictionary);
+    ~Word_MinMax_Player();
+    void getmove(int& x, int& y) override;
 };
 
 template <typename T>
-Pyramid_MinMax_Player<T>::Pyramid_MinMax_Player(T symbol) : Player<T>(symbol) {
+Word_MinMax_Player<T>::Word_MinMax_Player(T symbol, const unordered_set<string>& dictionary)
+    : Player<T>(symbol), dictionary(dictionary) {
     this->name = "AI Player";
+    for (char c = 'A'; c <= 'Z'; c++) allSymbols.push_back(c);
+    allSymbols.push_back(0);
 }
 
+
 template <typename T>
-void Pyramid_MinMax_Player<T>::getmove(int& x, int& y) {
-    pair<int, int> bestMove = getBestMove();
+void Word_MinMax_Player<T>::getmove(int& x, int& y) {
+
+    srand(static_cast<unsigned>(time(0)));
+    this->symbol = 'A' + rand() % 26;
+    auto bestMove = getBestMove(this->symbol);
     x = bestMove.first;
     y = bestMove.second;
 }
 
 template <typename T>
-int Pyramid_MinMax_Player<T>::calculateMinMax(T s, bool isMaximizing) {
-    if (this->boardPtr->is_win()) {
-        return isMaximizing ? -1 : 1;
-    }else if (this->boardPtr->is_draw()) {
-        return 0;
-    }
+int Word_MinMax_Player<T>::calculateMinMax(T symbol,bool isMaximizing) {
+    if (this->boardPtr->is_win()) return isMaximizing ? -1 : 1;
+    if (this->boardPtr->is_draw()) return 0;
 
     int bestValue = isMaximizing ? numeric_limits<int>::min() : numeric_limits<int>::max();
-    T opponentSymbol = (s == 'X') ? 'O' : 'X';
 
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < (2*i+1); j++) {
-            if (this->boardPtr->update_board(i, j, s)) {
-                int value = calculateMinMax(opponentSymbol, !isMaximizing);
+        for (int j = 0; j < 3; j++) {
+            if (this->boardPtr->update_board(i, j,  symbol)) {
+                int value = calculateMinMax(symbol,!isMaximizing);
                 this->boardPtr->update_board(i, j, 0);
                 if (isMaximizing) bestValue = max(bestValue, value);
-                 else bestValue = min(bestValue, value);
+                else bestValue = min(bestValue, value);
 
             }
         }
@@ -56,45 +65,18 @@ int Pyramid_MinMax_Player<T>::calculateMinMax(T s, bool isMaximizing) {
 }
 
 template <typename T>
-pair<int, int> Pyramid_MinMax_Player<T>::getBestMove() {
+std::pair<int, int> Word_MinMax_Player<T>::getBestMove(T symbol) {
     int bestValue = numeric_limits<int>::min();
     pair<int, int> bestMove = {-1, -1};
-    T opponentSymbol = (this->symbol == 'X') ? 'O' : 'X';
 
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < (2*i+1); j++) {
-            if (this->boardPtr->update_board(i, j, this->symbol)) {
-                if (this->boardPtr->is_win()) {
-                    this->boardPtr->update_board(i, j, 0);
-                    return {i, j};
-                }
-                this->boardPtr->update_board(i, j, 0);
-            }
-        }
-    }
-
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < (2*i+1); j++) {
-            if (this->boardPtr->update_board(i, j, opponentSymbol)) {
-                if (this->boardPtr->is_win()) {
-                    this->boardPtr->update_board(i, j, 0);
-                    return {i, j};
-                }
-                this->boardPtr->update_board(i, j, 0);
-            }
-        }
-    }
-
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < (2*i+1); j++) {
-            if (this->boardPtr->update_board(i, j, this->symbol)) {
-                int moveValue = calculateMinMax(this->symbol, false);
+        for (int j = 0; j < 3; j++) {
+            if (this->boardPtr->update_board(i, j, symbol)) {
+                int moveValue = calculateMinMax(symbol,false);
                 this->boardPtr->update_board(i, j, 0);
                 if (moveValue > bestValue) {
-                    bestMove = {i, j};
                     bestValue = moveValue;
+                    bestMove = {i, j};
                 }
             }
         }
@@ -104,18 +86,26 @@ pair<int, int> Pyramid_MinMax_Player<T>::getBestMove() {
 }
 
 
+template <typename T>
+bool Word_MinMax_Player<T>::isWinningMove(int x, int y, T symbol) {
+    this->boardPtr->update_board(x, y, symbol);
+    bool isWin = isWordFormed();
+    this->boardPtr->update_board(x, y, 0);
+    return isWin;
+}
 
 
+template <typename T>
+bool Word_MinMax_Player<T>::isWordFormed() {
+    vector<string> lines = getLines();
+    for (const auto& line : lines)
+        if (dictionary.find(line) != dictionary.end()) {return true;}
+
+    return false;
+}
 
 
+template <typename T>
+vector<string> Word_MinMax_Player<T>::getLines() {return this->boardPtr->get_lines();}
 
-
-
-
-
-
-
-
-
-
-#endif //_MINMAXPLAYER_H
+#endif // _WORD_MINMAX_PLAYER_H
